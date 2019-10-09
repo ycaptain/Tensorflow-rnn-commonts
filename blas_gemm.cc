cb938232c64beb417cea54a1e1274e91954e7a3f
yangyuyi <TaroYoung2000@gmail.com>
2019-10-10 07:57:03 +0800

    docs(blas_gemm): comments blas_gemm.h & blas_genn.cc

--- blas_gemm.cc
+++ blas_gemm.cc
@@ -13,18 +13,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+// DOC:
+//      是否使用多线程
 #define EIGEN_USE_THREADS
 
+// DOC:
+//      判断 GOOGLE_CUDA 或 TENSORFLOW_USE_ROCM 是否存在
+//      存在则引入相关头文件
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 #include "tensorflow/core/platform/stream_executor.h"
 #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/kernels/rnn/blas_gemm.h"
+// DOC:命名空间tensorflow
 namespace tensorflow {
 
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 namespace {
+    // DOC：
+    //  定义模版
+    //  目的:获取设备的可用内存信息
+    //  T -- 指针:记录cuda(GPU)可用的memory信息
 template <typename T>
 se::DeviceMemory<T> AsDeviceMemory(const T* cuda_memory) {
   se::DeviceMemoryBase wrapped(const_cast<T*>(cuda_memory));
@@ -34,7 +44,26 @@ se::DeviceMemory<T> AsDeviceMemory(const T* cuda_memory) {
 }  // namespace
 #endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 
+// DOC:命名空间 functor
 namespace functor {
+    // DOC:
+    //  实现矩阵的GEMM
+    //  定义泛型:方便处理不同size的矩阵
+    // 参数:
+    //  ctx -- 设定核方法内容的指针
+    //  transa -- 设定矩阵A是否转置
+    //  transb -- 设定矩阵B是否转置
+    //  m -- 矩阵A和矩阵C的行数
+    //  n -- 矩阵B和矩阵C的列数
+    //  k -- 矩阵A和矩阵B的列数
+    //  alpha -- 数乘系数
+    //  a -- 矩阵A
+    //  lda -- 矩阵A的递增步长
+    //  b -- 矩阵B
+    //  ldb -- 矩阵B的递增步长
+    //  beta -- 数乘系数
+    //  c -- 矩阵C 并将计算结果写入矩阵C
+    //  ldc -- 矩阵C的递增步长
 template <typename T>
 void TensorCuBlasGemm<T>::operator()(OpKernelContext* ctx, bool transa,
                                      bool transb, uint64 m, uint64 n, uint64 k,
@@ -45,10 +74,15 @@ void TensorCuBlasGemm<T>::operator()(OpKernelContext* ctx, bool transa,
   se::blas::Transpose trans[] = {se::blas::Transpose::kNoTranspose,
                                  se::blas::Transpose::kTranspose};
 
+  // DOC:
+  // 多个设备信息指针
   auto a_ptr = AsDeviceMemory(a);
   auto b_ptr = AsDeviceMemory(b);
   auto c_ptr = AsDeviceMemory(c);
 
+  // DOC:
+  //    采用ThenBlasGemm对矩阵进行计算
+  //    定义blas_launch_status 以标识计算是否成功
   bool blas_launch_status =
       ctx->op_device_context()
           ->stream()
@@ -57,7 +91,7 @@ void TensorCuBlasGemm<T>::operator()(OpKernelContext* ctx, bool transa,
           .ok();
   OP_REQUIRES(ctx, blas_launch_status, errors::Aborted("CuBlasGemm failed!"));
 #else
-  ctx->SetStatus(errors::InvalidArgument("CuBlasGemm needs CUDA."));
+  ctx->SetStatus(errors::InvalidArgument("CuBlasGemm needs CUDA.")); // 在没有安装CUDA时信息显示报错信息
 #endif
 }
 
--- blas_gemm.h
+++ blas_gemm.h
@@ -21,21 +21,30 @@ limitations under the License.
 #include "tensorflow/core/kernels/eigen_activations.h"
 #include "tensorflow/core/platform/types.h"
 
+// DOC:
+//      判断 GOOGLE_CUDA 或 TENSORFLOW_USE_ROCM 是否存在
+//      存在则引入相关头文件
 #if defined(TENSORFLOW_USE_CUSTOM_CONTRACTION_KERNEL)
 #include "tensorflow/core/kernels/eigen_contraction_kernel.h"
 #endif
 
+// DOC:命名空间tensorflow
 namespace tensorflow {
 class OpKernelContext;
+// DOC:命名空间functor
 namespace functor {
 
 template <typename T>
 struct TensorCuBlasGemm {
+    // DOC:
+    //  对矩阵A,矩阵B,矩阵C进行操作
   void operator()(OpKernelContext* ctx, bool transa, bool transb, uint64 m,
                   uint64 n, uint64 k, float alpha, const T* a, int lda,
                   const T* b, int ldb, float beta, T* c, int ldc);
 };
 
+// DOC:
+//      定义计算类型结构
 template <typename T>
 struct gemm_compute_type {
   typedef T type;
@@ -49,6 +58,8 @@ struct gemm_compute_type<Eigen::half> {
 template <typename Device, typename T, bool USE_CUBLAS>
 struct TensorBlasGemm;
 
+// DOC:
+//      使用GPU加速的GEMM实现
 template <typename Device, typename T>
 struct TensorBlasGemm<Device, T, true /* USE_CUBLAS */> {
   static void compute(OpKernelContext* ctx, const Device& d, bool transa,
@@ -57,6 +68,11 @@ struct TensorBlasGemm<Device, T, true /* USE_CUBLAS */> {
                       typename TTypes<T>::ConstMatrix b,
                       typename gemm_compute_type<T>::type beta,
                       typename TTypes<T>::Matrix c) {
+      // DOC:
+      //    获取m,n,k的值
+      //    将矩阵C第一维的大小赋值给m
+      //    将矩阵C第二维的大小赋值给n
+      //    根据矩阵A是否需要转置 确定k
     int64 m = c.dimensions()[0];
     int64 n = c.dimensions()[1];
     int64 k = transa ? a.dimensions()[0] : a.dimensions()[1];
@@ -67,6 +83,8 @@ struct TensorBlasGemm<Device, T, true /* USE_CUBLAS */> {
   }
 };
 
+// DOC:
+//      不使用GPU加速的GEMM实现
 template <typename Device, typename T>
 struct TensorBlasGemm<Device, T, false /* USE_CUBLAS */> {
   static void compute(OpKernelContext* ctx, const Device& d, bool transa,
@@ -75,9 +93,14 @@ struct TensorBlasGemm<Device, T, false /* USE_CUBLAS */> {
                       typename TTypes<T>::ConstMatrix b,
                       typename gemm_compute_type<T>::type beta,
                       typename TTypes<T>::Matrix c) {
+      // DOC:
+      //   矩阵A，矩阵B转置信息判断
     Eigen::array<Eigen::IndexPair<Eigen::DenseIndex>, 1> contract_pairs;
     contract_pairs[0] =
         Eigen::IndexPair<Eigen::DenseIndex>(transa == false, transb == true);
+    // DOC:
+    //      根据alpha beta对两个矩阵的数据类型进行判断 然后进行相应的计算操作
+    //      contract()张量tensor乘法，constant()定义常量的函数
     if (alpha == typename gemm_compute_type<T>::type(1.f) &&
         beta == typename gemm_compute_type<T>::type(0.f)) {
       c.device(d) = a.contract(b, contract_pairs);
